a0039bcb96d8616f73f529d9933c3843847abdf6
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-05 10:39:37 -0700

    [add] Two Sum II solution

--- 1.1-Array.py
+++ 1.1-Array.py
@@ -1,7 +1,10 @@
+from typing import List
+
 # 1 Given an array of 'n' nubers and a target value 't', find two numbers whose sum is 't':
 # Example: a=[5, 3, 6, 8, 2, 4, 7], t=10 => [3, 7] or [6, 4] or [8, 2]
 
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+def twoSum(numbers: List[int], target: int) -> List[int]:
     left, right = 0, len(numbers) - 1
     while left < right:
         if numbers[left] + numbers[right] > target:
@@ -9,15 +12,29 @@ def twoSum(self, numbers: List[int], target: int) -> List[int]:
         elif numbers[left] + numbers[right] < target:
             left += 1
         else:
-            return [left + 1, right + 1]
+            return [numbers[left], numbers[right]]
 
 
 arr = [5, 3, 6, 8, 2, 4, 7]
 t = 10
-two_sum(arr, t)
+print("Initial Values: ", arr, "target Value: ", t)
+print(twoSum(arr, t))
+
+
 
 # 2 Two Sum II - Input Array is sorted
 # Given a 1-indexed array of integers numbers that are already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length
+
+def twoSum2(numbers: List[int], target: int) -> List[int]:
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                print(numbers[i], numbers[j])
+
+arr2 = [1, 2, 5, 6, 7, 9, 11]
+t2 = 17
+twoSum2(arr2, t2)
+
 
 # 3 Given an array 'a' of 'n' numbers and a count 'k', find the 'k' largest values in the array 'a'.
 # Example: a=[5, 1, 3, 6, 8, 2, 4, 7], k=3  ⇒  [6, 8, 7]
