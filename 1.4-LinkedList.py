4863e2b660fdbfb493e0422b160b6a7da8fc574e
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-10 16:59:30 -0700

    [fix] change problem sets

--- 1.4-LinkedList.py
+++ 1.4-LinkedList.py
@@ -1,13 +1,14 @@
-# 1 Given a single-linked list, find the middle value in the list.
 
-# 2 Given a singly-linked list, reverse the order of the list by modifying the nodes’ links.
+# Example: If the given linked list is A → B → C → D → E, nodes should be modified/rearranged so the list becomes E → D → C → B → A.
 
-# 3 Given a singly-linked list, rearrange the nodes by interleaving the first half of the linked list with the second half.
+# 1. Given a singly-linked list, rearrange the nodes by interleaving the first half of the linked list with the second half.
+# Example: If the given linked list is A → B → C → D → E → F → G → H, nodes should be rearranged so the list becomes A → C → E → G → B → D → F → H.
 
-# 4 Rotate a given singly-linked list counter-clockwise by k nodes, where k is a given integer.
+# 2. Rotate a given singly-linked list counter-clockwise by k nodes, where k is a given integer.
+# Example: If the given linked list is A → B → C → D → E → F and k is 4, nodes should be modified so the list becomes E → F → A → B → C → D.
+# Assumptions: k is positive and smaller than n, the length of the linked list.
 
-# 5 Given an array of k singly-linked lists, each of whose values are in sorted order, combine all nodes (do not create new nodes) into one singly-linked list with all values in order.
+# 3. Given an array of k singly-linked lists, each of whose values are in sorted order, combine all nodes (do not create new nodes) into one singly-linked list with all values in order.
 
-# 6 Given a singly-linked list, find whether or not it contains a cycle, and if it does, find the node at which the cycle starts (the node that two other nodes reference/point to).
+# 4. New: Given a singly-linked list and an integer k, find the value in the kth-to-last node.
 
-# 7 Given a singly-linked list and an integer k, find the value in the kth-to-last node.
