be3996bf0daa3b451cb3d9652267f81da56aa614
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-12 00:07:46 -0700

    [feat] add code for reversed linked list

--- 1.4-LinkedList.py
+++ 1.4-LinkedList.py
@@ -1,5 +1,26 @@
+from LinkedList import LinkedList
 
 # Example: If the given linked list is A → B → C → D → E, nodes should be modified/rearranged so the list becomes E → D → C → B → A.
+regular_list = LinkedList()
+regular_list.append('A')
+regular_list.append('B')
+regular_list.append('C')
+regular_list.append('D')
+regular_list.append('E')
+
+# Example: If the given linked list is A → B → C → D → E, nodes should be modified/rearranged so the list becomes E → D → C → B → A.
+
+def reverse_linked_list(linked_list):
+    current = linked_list.head
+    previous = None
+    while current:
+        next_node, current.next = current.next, previous
+        previous, current = current, next_node
+    linked_list.head = previous
+    return linked_list
+
+print(reverse_linked_list(regular_list))
+
 
 # 1. Given a singly-linked list, rearrange the nodes by interleaving the first half of the linked list with the second half.
 # Example: If the given linked list is A → B → C → D → E → F → G → H, nodes should be rearranged so the list becomes A → C → E → G → B → D → F → H.
