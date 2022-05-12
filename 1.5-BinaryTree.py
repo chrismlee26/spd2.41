7bdfe7ae884c7279fb2af46479b5d80098d13a5d
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-12 15:37:32 -0700

    [test] add broken code for find range bst

--- 1.5-BinaryTree.py
+++ 1.5-BinaryTree.py
@@ -1,8 +1,42 @@
+from BinaryTree import BinaryTree
+
 # Binary Search Tree Problems
 
 # Problem 1
 # Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’ will be the first and last position of the ‘key’ in the array.
 # Write a function to return the range of the ‘key’. If the ‘key’ is not present return [-1, -1].
+# Input: 
+tree1 = BinaryTree()
+tree1.add(4)
+tree1.add(6)
+tree1.add(6)
+tree1.add(6)
+tree1.add(9)
+# arr = [4, 6, 6, 6, 9], 
+# key = 6
+
+# Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’ will be the first and last position of the ‘key’ in the array.
+def find_range_of_tree(tree1, target):
+    if tree1.root is None:
+        return [-1, -1]
+    else:
+        return find_range_of_tree_helper(tree1.root, target)
+
+    
+def find_range_of_tree_helper(node, target):
+    if node is None:
+        return [-1, -1]
+    if node.v == target:
+        return find_range_of_tree_helper(node.l, target)
+    elif node.v < target:
+        return find_range_of_tree_helper(node.r, target)
+    else:
+        return [node.v, node.v]
+
+    
+print("--------- Q1 ---------")
+print(find_range_of_tree(tree1, 6))
+
 
 # Examples
 # input1 = [4, 6, 6, 6, 9] key1 = 6
