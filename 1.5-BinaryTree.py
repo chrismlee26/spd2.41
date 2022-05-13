34a20e0a3eb885e47aa5cabd414784aa87a95dfe
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-13 13:55:53 -0700

    [add] bst problems, many not working :S

--- 1.5-BinaryTree.py
+++ 1.5-BinaryTree.py
@@ -16,10 +16,6 @@ tree1.printTree()
 arr = [4, 6, 6, 6, 9] 
 # key = 6
 
-# Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’ will be the first and last position of the ‘key’ in the array.
-# Write a function to return the range of the ‘key’. If the ‘key’ is not present return [-1, -1].
-# Input:
-
 def find_indexes(arr, target):
     if len(arr) == 0:
         return (-1, -1)
@@ -56,3 +52,47 @@ input = [1, 3, 8, 12, 4, 2]
 
 print("--------- Q1 ---------")
 print(find_max_bitonic(input))
+
+
+# Problem 3
+# Given a binary search tree, reverse the order of its values by modifying the nodes’ links.
+
+def reverse_bst(node):
+    if node is None:
+        return None
+    
+    node.left, node.right = node.right, node.left
+    
+    reverse_bst(node.left)
+    reverse_bst(node.right)
+
+    return node
+
+# Problem 4
+# Given a binary search tree containing integers and a target integer, come up with an efficient way to locate two nodes in the tree whose sum is equal to the target value.
+
+def find_sum_bst(node, target):
+    if node is None:
+        return None
+    
+    if node.data == target:
+        return node
+    
+    left = find_sum_bst(node.left, target - node.data)
+    right = find_sum_bst(node.right, target - node.data)
+    
+    if left is not None:
+        return left
+    return right
+
+# Problem 5 
+# Given a binary tree containing numbers, find the maximum sum path (the path that has the largest sum of node values). The path may start and end at any node in the tree.
+
+def find_max_path(node):
+    if node is None:
+        return 0
+    
+    left = find_max_path(node.left)
+    right = find_max_path(node.right)
+    
+    return max(left, right) + node.data
